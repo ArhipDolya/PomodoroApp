03c5ea23b66b7a8528e89ce9cce1adb33bcab06d
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-08-04 18:15:27 +0300

    Add pause/resume logic and button

--- PomodoroApp.py
+++ PomodoroApp.py
@@ -10,14 +10,15 @@ from sound import SOUND
 def play_alarm_sound():
     pygame.mixer.init()
     pygame.mixer.music.load(SOUND)
-    pygame.mixer.music.set_volume(0.4)
+    pygame.mixer.music.set_volume(0.3)
     pygame.mixer.music.play()
 
 
 def start_timer():
+    global TIMER_RUNNING
     total_seconds = int(hours.get()) * 3600 + int(minutes.get()) * 60 + int(seconds.get())
 
-    while total_seconds > 0:
+    while total_seconds > 0 and TIMER_RUNNING:
         hours.set(str(total_seconds // 3600).zfill(2))
         minutes.set(str((total_seconds % 3600) // 60).zfill(2))
         seconds.set(str(total_seconds % 60).zfill(2))
@@ -25,13 +26,23 @@ def start_timer():
         time.sleep(1)
         total_seconds -= 1
 
-    # When the timer is finished, it displays notifications
-    notification_title = "Pomodoro Timeer"
-    notification_message = "Hi :) Time is up! It's time to take a break!"
-    play_alarm_sound()
-    notification.notify(title=notification_title, message=notification_message, timeout=5)
+    if total_seconds == 0:
+        # When the timer is finished, it displays notifications
+        notification_title = "Pomodoro Timeer"
+        notification_message = "Hi :) Time is up! It's time to take a break!"
 
-    print("Timer finished!")
+        play_alarm_sound()
+        notification.notify(title=notification_title, message=notification_message, timeout=5)
+
+
+def pause_resume_timer():
+    global TIMER_RUNNING
+
+    if TIMER_RUNNING:
+        TIMER_RUNNING = False
+    else:
+        TIMER_RUNNING = True
+        start_timer()
 
 
 window = Tk()
@@ -70,8 +81,15 @@ minutesInput.place(x=X_INPUTS + 100, y=Y_INPUTS)
 secondsInput = Entry(window, width=3, font=('Arial', 20, ''), textvariable=seconds)
 secondsInput.place(x=X_INPUTS + 200, y=Y_INPUTS)
 
-start_button = Button(window, text='Start Pomodoro', font=('Arial', 18, 'bold'), command=start_timer)
-start_button.place(x=220, y=220)
+start_button = Button(window, text='Start Pomodoro', font=('Arial', 12, 'bold'), command=start_timer)
+start_button.place(x=100, y=210)
+
+
+TIMER_RUNNING = True
+
+pause_button = Button(window, text='Pause/Resume Pomodoro', font=('Arial', 12, 'bold'), command=pause_resume_timer)
+pause_button.place(x=250, y=210)
+
 
 
 if __name__ == '__main__':
